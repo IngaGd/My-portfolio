9d889dfc5b6c043ee6d70ef5273233e313145ba0
Inga <inga.gudaite@gmail.com>
2023-12-22 18:00:11 +0200

    Section about layout

--- apps/profile-app/src/components/About.jsx
+++ apps/profile-app/src/components/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import image from '../assets/images/profile/profile-7.png';
+import { FaChevronRight } from 'react-icons/fa6';
+
 export default function About() {
     return (
         <div className="about">
@@ -12,11 +15,94 @@ export default function About() {
                 assumenda quia id fuga.
             </p>
             <div className="about__content">
-                <div className="about__content--image"></div>
+                <div className="about__content--image">
+                    <img src={image} alt="Profile" />
+                </div>
                 <div className="about__content--description">
-                    <div className="heading-3">Web Developer</div>
-                    <div className="personal-info"></div>
-                    <div className="text"></div>
+                    <h2 className="heading-3">Full-Stack Developer</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vero consectetur sunt vitae ipsa dicta est, eaque
+                        excepturi consequatur hic quisquam corporis voluptate
+                        iure maxime eos quia maiores a molestias ipsam.
+                    </p>
+                    <div className="personal-info">
+                        <ul className="personal-info--list-left">
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Birthday: </b>1981 11 18
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Website: </b>inga-portfolio.lt
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Phone: </b> +370 656 09296
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>City: </b> Vilnius, Lithuania
+                                </p>
+                            </li>
+                        </ul>
+                        <ul className="personal-info--list-right">
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Age: </b>42
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Degree: </b>Master
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Email: </b>inga.gudaite@gmail.com
+                                </p>
+                            </li>
+                            <li className="list-item">
+                                <span>
+                                    <FaChevronRight />
+                                </span>
+                                <p>
+                                    <b>Freelance: </b>Available
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="text">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Esse corrupti laborum tempora ducimus maxime
+                        nesciunt, magnam consectetur! Voluptatem esse hic sunt,
+                        blanditiis ipsam quibusdam sapiente quisquam modi quo
+                        aliquid dolorem.
+                    </div>
                 </div>
             </div>
         </div>
