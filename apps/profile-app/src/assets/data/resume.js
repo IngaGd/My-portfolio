38b2420eeed2f719e9cb620afd89d1316e461a2d
Inga <inga.gudaite@gmail.com>
2023-12-26 21:06:45 +0200

    Resume section data, layout and style

---
+++ apps/profile-app/src/assets/data/resume.js
@@ -0,0 +1,108 @@
+const resumeList = [
+    {
+        id: 1,
+        sectionTitle: 'Summary',
+        content: [
+            {
+                title: 'Inga Gudaitė',
+                description: 'Full stack developer, 1 year experience in javascript, css, react, node.js',
+                date: '1981 11 18',
+                list: [
+                    { item: '+370 656 09296' },
+                    { item: 'inga.gudaite@gmail.com' },
+                    { item: 'https://www.linkedin.com/in/inga-gudaite/' },
+                    { item: 'https://github.com/IngaGd' }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        sectionTitle: 'Bootcamp',
+        content: [
+            {
+                title: 'Javascript',
+                description: 'Baltic institute of technology, Lithuania',
+                date: '2022 November - 2023 April',
+                list: [
+                    {
+                        item: 'Proficiency in HTML, CSS, and SASS styling, with a focus on creating responsive design.'
+                    },
+                    {
+                        item: 'In-depth understanding of JavaScript, including practical experience in working with arrays, strings, objects, and DOM manipulation.'
+                    },
+                    {
+                        item: 'Extensive experience with React, covering core concepts, context, hooks, and CRUD operations.'
+                    },
+                    {
+                        item: 'Utilising Express to implement HTTP methods such as GET, POST, DELETE, and UPDATE.'
+                    },
+                    { item: 'Leveraging the MariaDB platform for MySQL database operations.' },
+                    {
+                        item: ' Additionally, the Bootcamp offered invaluable experience working on both individual and team-based projects, fostering effective collaboration and project management skills.'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 3,
+        sectionTitle: 'Education',
+        content: [
+            {
+                title: 'Master of business economics',
+                description: 'Vilnius university, Lithuania',
+                date: '2011 - 2014',
+                list: []
+            },
+            {
+                title: 'Bachelor of mathematics',
+                description: 'Vilnius university, Lithuania',
+                date: '2000 - 2004',
+                list: []
+            }
+        ]
+    },
+    {
+        id: 4,
+        sectionTitle: 'Professional experience',
+        content: [
+            {
+                title: 'Freelancer',
+                description: 'Clients: Palm studio, MB Maistas',
+                date: '2023 Jully - Present',
+                list: [
+                    {
+                        item: 'Client Relationship Management'
+                    },
+                    {
+                        item: 'End-to-End Web Development'
+                    },
+                    {
+                        item: 'Database Management and Integration'
+                    },
+                    {
+                        item: 'Web Design and Styling'
+                    },
+                    { item: 'Deployment and Hosting' }
+                ]
+            },
+            {
+                title: 'Purchasing manager',
+                description: 'Vilniaus baldai, Lithuania',
+                date: '2015 November - 2021 November',
+                list: [
+                    { item: 'Building relationship with business partners' },
+                    {
+                        item: 'Supply chain management, ensuring best terms and conditions (negotiations) '
+                    },
+                    { item: 'Deep knowledge of the range of materials (technical requirements, market)' },
+                    { item: 'Mediation in product development' },
+                    { item: 'Execution, analysis and improvement of day-to-day activities and QA' }
+                ]
+            }
+        ]
+    }
+]
+
+export default resumeList;
--- apps/profile-app/src/components/Resume.jsx
+++ apps/profile-app/src/components/Resume.jsx
@@ -1,20 +1,57 @@
 import React from 'react';
+import resumeList from '../assets/data/resume';
 
 export default function Resume() {
+    // const list = resumeList;
     return (
         <div className="background grey">
-            <div className="resume  u-section-padding">
-                <div className="heading-2">Resume</div>
+            <div className="resume u-section-padding">
+                <h2 className="heading-2">Resume</h2>
                 <span className="underline"></span>
                 <p className="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quibusdam, voluptate atque enim ut, vitae sit voluptatum aut
-                    nam vero alias qui debitis facere reprehenderit minus magni
-                    assumenda quia id fuga.
+                    Junior Full-stack web developer specialising in SASS, React,
+                    NodeJS and MySQL. Executed and contributed to the full-stack
+                    web development projects, with an emphasis on front-end
+                    features and micro-services. For a
                 </p>
                 <div className="resume__content">
-                    <div className="resume__content--left">Left</div>
-                    <div className="resume__content-right">Right</div>
+                    {resumeList?.map((r, index) => (
+                        <div
+                            className={`resume__section--${index + 1}`}
+                            key={r.id}
+                        >
+                            <h3 className="heading-3">{r.sectionTitle}</h3>
+                            {r.content?.map((c, index) => (
+                                <div className="resume__text" key={index}>
+                                    <div className="resume__text--line-left"></div>
+                                    <div className="resume__text--column-right">
+                                        <div className="resume__text--title">
+                                            <span></span>
+                                            <h4 className="heading-4">
+                                                {c.title}
+                                            </h4>
+                                        </div>
+                                        <p className="resume__text--description">
+                                            {c.description}
+                                        </p>
+                                        <p className="resume__text--date">
+                                            {c.date}
+                                        </p>
+                                        {c.list?.map((l, index) => (
+                                            <ul
+                                                className="resume__text--list"
+                                                key={index}
+                                            >
+                                                <li className="skills">
+                                                    {l.item}
+                                                </li>
+                                            </ul>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
